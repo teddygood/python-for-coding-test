7db80c1ee27436e39437682e13e3d8a9e52d11cb
teddygood <ibear6954@gmail.com>
2022-08-01 06:35:10 +0900

    update chapter 6

---
+++ 6-10.py
@@ -0,0 +1,13 @@
+from re import L
+
+
+n = int(input())
+
+array = []
+for i in range(n):
+    array.append(int(input()))
+
+array = sorted(array, reverse=True)
+
+for i in array:
+    print(i, end=' ')
---
+++ 6-12.py
@@ -0,0 +1,16 @@
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+a.sort()
+b.sort(reverse=True)
+
+# 첫 번째 인덱스부터 확인하며, 두 배열의 원소를 최대 K번 비교
+for i in range(k):
+    # A의 원소가 B의 원소보다 작은 경우
+    if a[i] < b[i]:
+        a[i], b[i] = b[i], a[i]
+    else: # A의 원소가 B의 원소보다 크거나 같을 때, 반복 끝
+        break
+
+print(sum(a))
